74d4a0b33f8269908a144c1006129fe7238fa466
aledu <aledu@AsusAle.mshome.net>
2023-02-22 08:02:25 -0600

    Se agregan las plantillas del layout, index y modificacion de datos. Tambien se agregan las dependencias faltantes en el pom (springboot jpa y mysql) y se corrige el estadoController.java

--- src/main/java/com/practica01/controller/EstadoController.java
+++ src/main/java/com/practica01/controller/EstadoController.java
@@ -10,31 +10,45 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 
 @Controller
 @Slf4j
 public class EstadoController {
     
-    @Autowired
+    @Autowired//esta etiqueta se da cuenta de que EstadoService es una interface, entonces busca que clase usa
     private EstadoService estadoService;
     
     @GetMapping("/")
     public String inicio(Model model){
-        var variable="Hola desde el back-End";
-        model.addAttribute("mensaje", variable);
-        
         var estados=estadoService.getEstados();
         model.addAttribute("estados", estados);
         return "index";
-        
     }
-         @GetMapping("/estado/nuevo")
-        public String estadonuevo(Model model){
-           
-            
-            return "modificaestado";
-        
+    
+    @GetMapping("/estado/nuevo")
+    public String estadoNuevo(Estado estado){
+        return "modificaEstado";
+    }
+    
+    @PostMapping("/estado/guardar")
+    public String estadoGuardar(Estado estado){
+        estadoService.save(estado);
+        return "redirect:/";
+    }
+    
+    @GetMapping("/estado/eliminar/{idEstado}")
+    public String estadoEliminar(Estado estado){
+        estadoService.delete(estado);
+        return "redirect:/";
+    }
+    
+    @GetMapping("/estado/modificar/{idEstado}")
+    public String estadoModificar(Model model, Estado estado){
+        estado=estadoService.getEstado(estado);
+        model.addAttribute("estado", estado);
+        return "modificaEstado";
     }
     
     
